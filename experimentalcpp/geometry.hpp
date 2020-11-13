f0d09f0ecc735546d5225dd0a735bc24110bd585
Lucas Crijns <lucascrijns@gmail.com>
2020-11-13 16:24:14 +0100

    Cpp simulation stub

---
+++ experimentalcpp/geometry.hpp
@@ -0,0 +1,11 @@
+#ifndef GEOMETRY
+#define GEOMETRY
+
+
+struct vec3_ {
+    float x, y, z;
+}
+
+
+typedef struct vec3_ vec3;
+#endif
---
+++ experimentalcpp/tree.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+#include "geometry.hpp"
+
+class OctNode {
+private:
+    float center, size;
+    std::vector<Octnode*> children;
+public:
+    Octnode()
+}
