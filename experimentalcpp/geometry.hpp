b1165036c83ab385251c02e16c0f2c67366bca9b
Lucas Crijns <lucascrijns@gmail.com>
2020-11-13 16:46:57 +0100

    Updated geometry

--- experimentalcpp/geometry.hpp
+++ experimentalcpp/geometry.hpp
@@ -1,11 +1,13 @@
 #ifndef GEOMETRY
 #define GEOMETRY
 
+#include <valarray>
 
-struct vec3_ {
-    float x, y, z;
+typedef valarray<float> vec3;
+
+vec3 zeros3() {
+    return vec3();
 }
-
 
 typedef struct vec3_ vec3;
 #endif
