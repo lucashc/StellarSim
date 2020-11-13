39b9f336d82449b6864fb04d33c6e6c77613b6bd
Benjamin <benjamin.oudejans@gmail.com>
2020-11-13 23:29:09 +0100

    Minor fix in determining i,j,k

--- experimentalcpp/tree.cpp
+++ experimentalcpp/tree.cpp
@@ -28,9 +28,9 @@ public:
 
         for(int octant_index = 0; octant_index < 8, ++octant_index){  // create child nodes for each octant
             if(octant_points[octant_index].size() == 0){continue}
-            float i = (octant_index>>2) % 2;  // not very elegant, but reverse-engineers i,j,k from octant index
+            float i = octant_index % 2;  // not very elegant, but reverse-engineers i,j,k from octant index
             float j = (octant_index>>1) % 2;  // which we need to calculate the offset dx of the child node
-            float k = octant_index % 2;
+            float k = (octant_index>>2) % 2;
             dx = 0.5*this->size*(vec3(i-0.5,j-0.5,k-0.5));
             this->children.push_back(OctNode(center + dx, this->size/2, octant_masses[octant_index],
                                              octant_points[octant_index], octant_ids[octant_index], leaves));
