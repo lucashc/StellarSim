5a71a6060c0bac8e6ca32a545ff47d388f9bfd58
Benjamin <benjamin.oudejans@gmail.com>
2020-11-16 11:32:28 +0100

    Treewalk update

--- experimentalcpp/treewalk.cpp
+++ experimentalcpp/treewalk.cpp
@@ -7,7 +7,7 @@ void TreeWalk(OctNode node, OctNode node0, float thetamax, float G){
     float dx = node.COM - node0.COM;
     float r = sqrt((dx*dx).sum());
     if (r>0){
-        if(node.children.size() ==0 || node.size/r < thetamax){
+        if(node.children.empty() || node.size/r < thetamax){
             node0.g = node0.g + G * node.mass * dr/r**3
         }
         else{
