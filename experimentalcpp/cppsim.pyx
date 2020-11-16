8b7440a3b680654c2ac92045a745994a0b914f4a
Lucas Crijns <lucascrijns@gmail.com>
2020-11-17 00:56:10 +0100

    Added numpy support

--- experimentalcpp/cppsim.pyx
+++ experimentalcpp/cppsim.pyx
@@ -1,5 +1,6 @@
 # distutils: language=c++
 import numpy as np
+cimport numpy as np
 from libcpp.vector cimport vector
 
 cdef extern from "basetypes.hpp":
@@ -19,26 +20,22 @@ cdef extern from "body.hpp":
 
 cdef class Body3:
 
-    cdef Body body
-
-    def __init__(self, pos=[0,0,0], vel=[0,0,0], mass=0):
-        cdef vec3 pos3, vel3
-        pos3 = vec3(pos[0], pos[1], pos[2])
-        vel3 = vec3(vel[0], vel[1], vel[2])
-        self.body = Body(pos3, vel3, mass)
+    cdef Body *body
+    def __cinit__(self, np.ndarray[double] pos=np.array([0,0,0], dtype=np.double), np.ndarray[double] vel=np.array([0,0,0], dtype=np.double), double mass=0):
+        self.body = new Body(vec3(pos[0], pos[1], pos[2]), vec3(vel[0], vel[1], vel[2]), mass)
     
     @property
     def mass(self):
         return self.body.mass
     @mass.setter
-    def mass(self, mass):
+    def mass(self, double mass):
         self.body.mass = mass
     
     @property
     def pos(self):
         return np.array([self.body.pos.x, self.body.pos.y, self.body.pos.z])
     @pos.setter
-    def pos(self, pos):
+    def pos(self, np.ndarray[double] pos):
         self.body.pos.x = pos[0]
         self.body.pos.y = pos[1]
         self.body.pos.z = pos[2]
@@ -47,21 +44,28 @@ cdef class Body3:
     def vel(self):
         return np.array([self.body.vel.x, self.body.vel.y, self.body.vel.z])
     @vel.setter
-    def vel(self, vel):
+    def vel(self, np.ndarray[double] vel):
         self.body.vel.x = vel[0]
         self.body.vel.y = vel[1]
         self.body.vel.z = vel[2]
     
     def __repr__(self):
-        return f"Body(pos=[{self.body.pos.x}, {self.body.pos.y}, {self.body.pos.z}], vel=[{self.body.vel.x}, {self.body.vel.y}, {self.body.vel.z}], mass={self.body.mass})"
+        return f"Body3(pos=[{self.body.pos.x}, {self.body.pos.y}, {self.body.pos.z}], vel=[{self.body.vel.x}, {self.body.vel.y}, {self.body.vel.z}], mass={self.body.mass})"
     
     def __str__(self):
         return  self.__repr__()
+    
+    def __dealloc__(self):
+        del self.body
+    cdef Body* get_ptr(self):
+        return self.body.get_ptr()
+
+Body3_t = np.dtype(Body3)
 
 cdef class BodyList3:
     cdef bodylist bl
 
-    def __init__(self, bodies):
-        # Assume bodies is list or numpy array
-        for i in bodies:
-            pass
+    def __init__(self, np.ndarray[Body3_t] b):
+        if b.dtype != Body3_t:
+            raise TypeError("Not a Body3 type")
+        
--- experimentalcpp/setup.py
+++ experimentalcpp/setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup
 from Cython.Build import cythonize
+import numpy
 
 setup(ext_modules=cythonize(
     '*.pyx',
     language_level=3
-))
+), include_dirs=[numpy.get_include()])
