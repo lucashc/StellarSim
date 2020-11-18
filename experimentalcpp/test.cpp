ae2f1c81a227475a677b8704d938579a1564256c
Benjamin <benjamin.oudejans@gmail.com>
2020-11-18 15:56:44 +0100

    Changed initial state of bodies

--- experimentalcpp/test.cpp
+++ experimentalcpp/test.cpp
@@ -6,9 +6,12 @@
 using namespace std;
 
 int main() {
-    auto p = vector<vec3> {vec3(0, 0, 0), vec3(2, 1, 0), vec3(-1, -1, 0), vec3(0, 2, 1) };
-    auto m = scalist({100.0, 1.0, 2.0, 123456});
-    auto v = vector<vec3>{vec3(0, 0, 0), vec3(0, 0, 0), vec3(0, 0, 0), vec3(0, 0, 0) };
+    //auto p = vector<vec3> {vec3(0, 0, 0), vec3(2, 1, 0), vec3(-1, -1, 0), vec3(0, 2, 1) };
+    //auto m = scalist({100.0, 1.0, 2.0, 123456});
+    //auto v = vector<vec3>{vec3(0, 0, 0), vec3(0, 0, 0), vec3(0, 0, 0), vec3(0, 0, 0) };
+    auto p = vector<vec3> {vec3(0,0,0), vec3(50,0,0), vec3(0,50,0), vec3(-100,-100,0)};
+    auto v = vector<vec3> {vec3(0,0,0), vec3(0,100,50), vec3(-100,0,20), vec3(-10,30,0)};
+    auto m = scalist({1e6, 1, 1e4, 10});
     veclist p_, v_;
     for(int i = 0; i<p.size(); i++){
         p_.push_back(&p[i]);
