10a39bcec6e60251d1af15f9c1cb5fd0f58d99e6
Lucas Crijns <lucascrijns@gmail.com>
2020-11-18 14:54:43 +0100

    Make bounding vectors compatible with bodies

--- experimentalcpp/sim.cpp
+++ experimentalcpp/sim.cpp
@@ -5,11 +5,7 @@
 
 
 void accelerations(bodylist &bodies, BASETYPE thetamax, BASETYPE G) {
-    veclist points;
-    for (auto body : bodies) {
-        points.push_back(&body->pos);
-    }
-    auto bounds = get_bounding_vectors(points);
+    auto bounds = get_bounding_vectors(bodies);
     auto center = (bounds.first + bounds.second)/2;
     BASETYPE max_size = (bounds.first-bounds.second).abs().max();
     std::cout << max_size << std::endl;
