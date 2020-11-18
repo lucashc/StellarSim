9222f03554e8725b305a97199aef87a6dbed85ae
Lucas Crijns <lucascrijns@gmail.com>
2020-11-18 17:44:43 +0100

    Plotting works!

--- experimentalcpp/test.py
+++ experimentalcpp/test.py
@@ -5,32 +5,36 @@ from matplotlib import animation
 
 
 N = 4
+until_timestep = 5000
 r0 = np.array([[0, 0, 0], [50, 0, 0], [0, 50, 0], [-100, -100, 0]], dtype=np.double)
 v0 = np.array([[0, 0, 0], [0, 100, 50], [-100, 0, 20], [-10, -30, 0]], dtype=np.double)
 m = np.array([1e6, 1, 10000, 10], dtype=np.double)
 bodies = np.array([cs.Body3(r0[i], v0[i], m[i]) for i in range(N)])
 bodylist = cs.BodyList3(bodies)
 
+result = cs.EulerForwardSaveC(bodylist, 1e-2, until_timestep, 0.5, 1)
 
-result = cs.EulerForwardSaveC(bodylist, 10, 100, 0.5, 1)
+s = np.empty((until_timestep, N, 3))
+for i in range(until_timestep):
+    for j in range(N):
+        s[i, j, :] = result[i,j].pos
 
-exit()
 
-until_timestep = 100
-large_limits = {"xlim": (-140, 70), "ylim": (-120, 60), "zlim":(-15, 15)}
+large_limits = {"xlim": (-1000, 1000), "ylim": (-1000, 1000), "zlim":(-1000, 1000)}
 sun_limits = {"xlim": (-20, 1), "ylim": (-10, 10), "zlim": (-1, 5)}
 starting_angle = 0  # default 270, 0 for sun zoom
 rotation_speed = 40  # default 40
 elevation = 0  # default 0
+
 def data_gen(index):
     ax.clear()
     #ax.axis('off')
     ax.grid('off')
-    plot0 = ax.plot3D(s.y[0][:index], s.y[1][:index], s.y[2][:index], linewidth=2)
-    plot1 = ax.plot3D(s.y[6][:index], s.y[7][:index], s.y[8][:index])
-    plot2 = ax.plot3D(s.y[12][:index], s.y[13][:index], s.y[14][:index])
-    plot3 = ax.plot3D(s.y[18][:index], s.y[19][:index], s.y[20][:index])
-    ax.set(**sun_limits)
+    plot0 = ax.plot3D(s[:index, 0, 0], s[:index, 0, 1], s[:index, 0, 2], linewidth=2)
+    plot1 = ax.plot3D(s[:index, 1, 0], s[:index, 1, 1], s[:index, 1, 2])
+    plot2 = ax.plot3D(s[:index, 2, 0], s[:index, 2, 1], s[:index, 2, 2])
+    plot3 = ax.plot3D(s[:index, 3, 0], s[:index, 3, 1], s[:index, 3, 2])
+    ax.set(**large_limits)
     ax.view_init(elev=elevation, azim=index/until_timestep*rotation_speed + starting_angle)
     return plot0, plot1, plot2, plot3
 
@@ -50,10 +54,9 @@ ax = fig.add_subplot(1, 1, 1, projection='3d')
 grav_ani = animation.FuncAnimation(fig, data_gen, frames=np.arange(0, until_timestep, 10),
                               interval=30, blit=False)
 
-plt.rcParams['animation.ffmpeg_path'] = 'C:/FFmpeg/bin/ffmpeg.exe'
 Writer = animation.writers['ffmpeg']
-writer = Writer(fps=20, metadata=dict(artist='Benjamin Oudejans'), bitrate=1800)
-filename = 'C:/Users/benja/Pictures/MathViz/grav_ani_sunzoom.mp4'
+writer = Writer(fps=20, metadata=dict(artist='Lucas Crijns'), bitrate=1800)
+filename = 'test.mp4'
 print('0%')
 # begin = time.time()
 grav_ani.save(filename, writer=writer)
