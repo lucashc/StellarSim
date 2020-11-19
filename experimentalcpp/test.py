67c2cc696dcff4d52878be35459aadf8c6bd78d3
Lucas Crijns <lucascrijns@gmail.com>
2020-11-19 11:31:07 +0100

    Added circular orbit

--- experimentalcpp/test.py
+++ experimentalcpp/test.py
@@ -4,24 +4,25 @@ import matplotlib.pyplot as plt
 from matplotlib import animation
 
 
-N = 4
-until_timestep = 5000
-r0 = np.array([[0, 0, 0], [50, 0, 0], [0, 50, 0], [-100, -100, 0]], dtype=np.double)
-v0 = np.array([[0, 0, 0], [0, 100, 50], [-100, 0, 20], [-10, -30, 0]], dtype=np.double)
-m = np.array([1e6, 1, 10000, 10], dtype=np.double)
+N = 2
+until_timestep = int(1e5)
+r0 = np.array([[-50, 0, 0], [50, 0, 0]], dtype=np.double)
+v0 = np.array([[0, -0.7, 0], [0, 0.7, 0]], dtype=np.double)
+m = np.array([100, 100], dtype=np.double)
 bodies = np.array([cs.Body3(r0[i], v0[i], m[i]) for i in range(N)])
 bodylist = cs.BodyList3(bodies)
 
-result = cs.EulerForwardSaveC(bodylist, 1e-2, until_timestep, 0.5, 1)
+result = cs.EulerForwardSaveC(bodylist, 1e-2, until_timestep, 1, 1)
+
+print(result[2500,1])
 
 s = np.empty((until_timestep, N, 3))
 for i in range(until_timestep):
     for j in range(N):
         s[i, j, :] = result[i,j].pos
 
-
-large_limits = {"xlim": (-1000, 1000), "ylim": (-1000, 1000), "zlim":(-1000, 1000)}
-sun_limits = {"xlim": (-20, 1), "ylim": (-10, 10), "zlim": (-1, 5)}
+large_limits = {"xlim": (-200, 200), "ylim": (-200, 200), "zlim":(-200, 200)}
+sun_limits = {"xlim": (-100, 100), "ylim": (-100, 100), "zlim": (-100, 100)}
 starting_angle = 0  # default 270, 0 for sun zoom
 rotation_speed = 40  # default 40
 elevation = 0  # default 0
@@ -32,11 +33,11 @@ def data_gen(index):
     ax.grid('off')
     plot0 = ax.plot3D(s[:index, 0, 0], s[:index, 0, 1], s[:index, 0, 2], linewidth=2)
     plot1 = ax.plot3D(s[:index, 1, 0], s[:index, 1, 1], s[:index, 1, 2])
-    plot2 = ax.plot3D(s[:index, 2, 0], s[:index, 2, 1], s[:index, 2, 2])
-    plot3 = ax.plot3D(s[:index, 3, 0], s[:index, 3, 1], s[:index, 3, 2])
-    ax.set(**large_limits)
+    # plot2 = ax.plot3D(s[:index, 2, 0], s[:index, 2, 1], s[:index, 2, 2])
+    # plot3 = ax.plot3D(s[:index, 3, 0], s[:index, 3, 1], s[:index, 3, 2])
+    ax.set(**sun_limits)
     ax.view_init(elev=elevation, azim=index/until_timestep*rotation_speed + starting_angle)
-    return plot0, plot1, plot2, plot3
+    return plot0, plot1
 
 
 # preview a frame
