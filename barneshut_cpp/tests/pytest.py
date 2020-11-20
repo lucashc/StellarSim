97e47e5b84079a0bbe9a23a4ce1ed94032591230
Lucas Crijns <lucascrijns@gmail.com>
2020-11-20 17:41:59 +0100

    Add reading and writing of bodylists

--- barneshut_cpp/tests/pytest.py
+++ barneshut_cpp/tests/pytest.py
@@ -14,6 +14,23 @@ class PyTest(unittest.TestCase):
     def test_body(self):
         import cppsim as cs
         self.assertTrue((cs.Body3().pos == np.array([0,0,0], dtype=np.double)).all())
+    
+    def test_reading(self):
+        import cppsim as cs
+        os.system("./build/bodytest.elf")
+        bl = cs.BodyList3.load("test.bin")
+    
+    def test_saving(self):
+        import cppsim as cs
+        import numpy as np
+        x = cs.BodyList3(np.array([
+            cs.Body3(),
+            cs.Body3(np.array([1,2,3], dtype=np.double))
+        ]))
+        x.save("test2.bin")
+        result = cs.BodyList3.load("test2.bin")
+        self.assertTrue((x[0].pos == result[0].pos).all())
+        self.assertTrue((x[1].pos == result[1].pos).all())
 
 class CppTest(unittest.TestCase):
 
