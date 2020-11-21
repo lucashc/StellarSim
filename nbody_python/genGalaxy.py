bb58a29bb1d677ebc15107bc5b980c531ce09c1c
PimKeer <pim.keer@gmail.com>
2020-11-21 13:32:39 +0100

    Setup for genGalaxy function, which will generate galaxies.

---
+++ nbody_python/genGalaxy.py
@@ -0,0 +1,5 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+def genGalaxy(n,M,):
+    """Generate a galaxy of a massive black hole and n stars, with initial positions, velocities and masses randomly distributed"""
