1e8cb15d46e0a8fa1f1452106cbfd6fae18f38dd
Sven Holtrop <yadeses@gmail.com>
2020-11-23 15:56:13 +0100

    time parallellization

--- Scenarios/rendering_test.py
+++ Scenarios/rendering_test.py
@@ -8,9 +8,10 @@ import helper_files.render as render
 
 thetamax = 0.5
 G = 1
-n_steps = 1500
-dt = 1e-1
-
+n_steps = 1000
+save_step = 10
+dt = 1e-2
+'''
 center1 = np.array([400, 0, 0])
 center2 = np.array([-400, 0, 0])
 v1 = np.array([-3, 0.6, 0])
@@ -47,11 +48,15 @@ transform = np.array([[0, 1, 0],
                     [0, 0, 1]])
 radii = np.linalg.norm(positions, axis=1)
 velocities = np.tensordot(positions, transform, axes=1)/radii[..., np.newaxis]
-'''
+
 total_bodylist = utils.zip_to_bodylist(positions, velocities, masses)
 total_bodylist.check_integrity()
-results = cs.LeapFrogSaveC(total_bodylist, dt, n_steps, thetamax, G)
-print("done")
+
+begin = time.time()
+results = cs.LeapFrogSaveC(total_bodylist, dt, n_steps, thetamax, G, save_step)
+end = time.time()
+print("Simulation finished after", end-begin, "s")
+
 s = utils.get_positions(results)
 plane = render.Plane(np.array([0, 0, 1]), np.array([0, 0, 0]), np.array([1/8, 0, 0]), np.array([0, 1/8, 0]))
 render.animate(s, masses, plane, 400, 400)
