7792d1968f02c15730e46d2f7e3dc27199f850e8
PimKeer <pim.keer@gmail.com>
2020-11-26 15:34:36 +0100

    Added function to retrieve energy.

--- Scenarios/PhysQuants.py
+++ Scenarios/PhysQuants.py
@@ -6,6 +6,7 @@ import helper_files.sim_utils as utils
 def mass(result,t=0):
     masses = utils.get_masses(result)[t]
     blackholemass = masses[0]
+
     return masses, sum(masses), blackholemass/sum(masses)
 
 def linMom(result, t=0):
@@ -14,8 +15,10 @@ def linMom(result, t=0):
     v_x, v_y, v_z = v[:,0], v[:,1], v[:,2]
     p_x, p_y, p_z = masses * v_x, masses * v_y, masses * v_z
     p = np.column_stack((p_x,p_y,p_z))
+    pmag = np.sqrt((p_x)**2+(p_y)**2+(p_z)**2)
     sump = np.array([sum(p_x), sum(p_y), sum(p_z)])
-    return p, sump, np.linalg.norm(sump)
+
+    return p, sump, np.linalg.norm(sump), pmag
 
 def angMom(result, t=0):
     masses = utils.get_masses(result)[t]
@@ -24,4 +27,21 @@ def angMom(result, t=0):
     L = np.cross(r,p)
     L_x, L_y, L_z = L[:,0], L[:,1], L[:,2]
     sumL = np.array([sum(L_x), sum(L_y), sum(L_z)])
+
     return L, sumL, np.linalg.norm(sumL)
+
+def energy(result,t=0):
+    masses = utils.get_masses(result)[t]
+    r = utils.get_positions(result)[t]
+    pmag = linMom(result,t)[3]
+
+    Ekin = sum(pmag**2/(2*masses))
+
+    Epot = 0
+    for i in range(len(masses)):
+        for j in range(i+1,len(masses)):
+            dist = np.linalg.norm(r[i]-r[j])
+            Epot += sc.G*masses[i]*masses[j]/dist
+
+    return Ekin+Epot, Ekin, Epot
+
