a3fee3c5844dad8f3cd53cb94b40a3fde50eabd0
Sven Holtrop <yadeses@gmail.com>
2020-11-30 17:54:18 +0100

    Semi stable galaxy

--- Scenarios/GaussGalaxy.py
+++ Scenarios/GaussGalaxy.py
@@ -18,15 +18,20 @@ def genStableGalaxy(n_stars, m_star, m_bh):
     v_unit_vec = np.column_stack((-np.sin(theta), np.cos(theta), np.zeros(n_stars)))
     velocities = v_norm.reshape((n_stars, 1)) * v_unit_vec
     positions = np.insert(positions, 0, np.zeros(3), 0)     # add black hole (already present in masses)
-    velocities = np.insert(velocities, 0, np.zeros(3), 0)
+    velocities = np.insert(velocities, 0, -np.sum(velocities*m_star, axis=0)/m_bh, 0)
+    print(velocities)
+    print(velocities*np.expand_dims(masses, axis=0).T)
+    print(np.sum(velocities*np.expand_dims(masses, axis=0).T, axis=0))
     return utils.zip_to_bodylist(positions, velocities, masses)
 
 
 thetamax = 0.7
-n_steps = 1000
+n_steps = 5000
 m_star = sc.Msol  # 3.181651515706176e+30
 galaxy = genStableGalaxy(10000, m_star, sc.Msgra)
-result = cs.LeapFrogSaveC(galaxy, 1e12, n_steps, thetamax, sc.G)
+cs.LeapFrogC(galaxy, 1e12, 5000, thetamax, sc.G)
+print("Done with step 1")
+result = cs.LeapFrogSaveC(galaxy, 1e12, n_steps, thetamax, sc.G, 10)
 result.save("stable_test.binv")
 
 # result = cs.Result.load("stable2.binv").numpy()
