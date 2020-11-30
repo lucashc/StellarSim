90764d7c32e46d7d71b383063a41e98db73efe8f
Lucas Crijns <lucascrijns@gmail.com>
2020-11-30 20:33:44 +0100

    Appended _source to allow documenting code

--- barneshut_cpp/setup.py
+++ barneshut_cpp/setup.py
@@ -3,6 +3,6 @@ from Cython.Build import cythonize
 import numpy
 
 setup(ext_modules=cythonize(
-    'cppsim/cppsim.pyx',
+    'cppsim_source/cppsim.pyx',
     language_level=3
 ), include_dirs=[numpy.get_include(), './'])
