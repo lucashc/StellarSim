0d5a78dbd29909d5fcac5aba1dcaeff7c5ab6357
Lucas Crijns <lucascrijns@gmail.com>
2021-01-01 13:39:19 +0100

    Added pause option to visualisation

--- helper_files/vispy.py
+++ helper_files/vispy.py
@@ -26,6 +26,7 @@ Keybinding:
 * F11 Set fullscreen
 * `=` Increase pointsize by 1
 * `-` Decrease pointsize by 1
+* `spacebar` Pause and continue
 """
 
 
@@ -105,6 +106,8 @@ can_record = args.record != '-'
 filename = args.record
 frames = []
 fps = 1
+paused = False
+lag = 0
 
 # fps setter
 def set_fps(result):
@@ -114,9 +117,12 @@ def set_fps(result):
 
 # Handle update
 def update_vertices(ev, *args):
-    global vis, timescale, canvas, frames
-    index = int(ev.count / timescale) % vis.frames
-    vis.set_vertex_data(index)
+    global vis, timescale, canvas, frames, lag, paused
+    if not paused:
+        index = int((ev.count-lag) / timescale) % vis.frames
+        vis.set_vertex_data(index)
+    else:
+        lag += 1
     if record:
         im = canvas.render()
         frames.append(im)
@@ -137,7 +143,7 @@ def write_recording():
 # Handle speedup
 @canvas.events.key_press.connect
 def handle_key(ev):
-    global timescale, record, can_record, frames, filename, canvas
+    global timescale, record, can_record, frames, filename, canvas, paused
     if ev.text == ']':
         # Increase speed
         timescale /= 2
@@ -146,7 +152,9 @@ def handle_key(ev):
         timescale *= 2
     elif ev.text == 'r':
         # Start record
-        if can_record:
+        if record:
+            print("Already recording")
+        elif can_record:
             record = True
             print("Started recording")
             # Measure fps
@@ -169,5 +177,11 @@ def handle_key(ev):
         vis.pointsize += 1
     elif ev.text == '-':
         vis.pointsize -= 1
+    elif ev.text == ' ':
+        if paused:
+            print("Unpaused")
+        else:
+            print("Paused")
+        paused = not paused
 
 app.run()
