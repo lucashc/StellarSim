bceca06e0e604125f06d0b606c068c41e50e229c
Lucas Crijns <lucascrijns@gmail.com>
2021-01-07 19:02:36 +0100

    Working with vis

--- GPU/GPU.py
+++ GPU/GPU.py
@@ -2,39 +2,97 @@ import numpy as np
 import pyopencl as cl
 import pyopencl.array
 from pyopencl.elementwise import ElementwiseKernel
+from .types import body
+from tqdm import tqdm
 
 
 class GPU:
     def __init__(self):
         self.ctx = cl.create_some_context()
         self.queue = cl.CommandQueue(self.ctx)
+        
+        # Register custom body type
+        body_struct, body_struct_c = cl.tools.match_dtype_to_c_struct(self.ctx.devices[0], 'body', body)
+        cl.tools.get_or_register_dtype('body', body_struct)
 
-    def applyAccelerations(self, b):
-        accelerations = ElementwiseKernel(self.ctx, 
-            "int n_bodies, int* index_g, float *bodies, float *acc",
+        # kernels
+        self.getAccelerations = ElementwiseKernel(self.ctx, 
+            "int n_bodies, body *bodies, double *acc, double G",
             """
-            float gx = 0, gy = 0, gz = 0;
+            double gx = 0, gy = 0, gz = 0;
             for (int j = 0; j < n_bodies; j++) {
-                printf("%d,%d", i, j);
                 if (i == j) continue;
-                float rx = bodies[i, 0]-bodies[j, 0];
-                float ry = bodies[i, 1]-bodies[j, 1];
-                float rz = bodies[i, 2]-bodies[j, 2];
-                float dr = rsqrt(rx*rx + ry*ry + rx*rz);
-                float dr3 = dr * dr * dr;
-                gx += rx * bodies[j, 6] * dr3;
-                gy += ry * bodies[j, 6] * dr3;
-                gz += rz * bodies[j, 6] * dr3;
+                double rx = bodies[j].pos[0]-bodies[i].pos[0];
+                double ry = bodies[j].pos[1]-bodies[i].pos[1];
+                double rz = bodies[j].pos[2]-bodies[i].pos[2];
+                double dr = rsqrt(rx*rx + ry*ry + rz*rz);
+                double dr3 = dr * dr * dr;
+                gx += G * rx * bodies[j].mass * dr3;
+                gy += G * ry * bodies[j].mass * dr3;
+                gz += G* rz * bodies[j].mass * dr3;
             }
-            acc[i, 0] = gx;
-            acc[i, 1] = gy;
-            acc[i, 2] = gz;
+            acc[i*3 + 0] = gx;
+            acc[i*3 + 1] = gy;
+            acc[i*3 + 2] = gz;
             """,
-            "accelerations"
+            "get_accelerations", preamble=body_struct_c
         )
-        index = np.arange(0, len(b)).astype(np.int32)
-        index_g = 
+
+        self.updateVelocities = ElementwiseKernel(self.ctx, 
+            "int n_bodies, body *bodies, double G, double dt",
+            """
+            double gx = 0, gy = 0, gz = 0;
+            for (int j = 0; j < n_bodies; j++) {
+                if (i == j) continue;
+                double rx = bodies[j].pos[0]-bodies[i].pos[0];
+                double ry = bodies[j].pos[1]-bodies[i].pos[1];
+                double rz = bodies[j].pos[2]-bodies[i].pos[2];
+                double dr = sqrt(rx*rx + ry*ry + rz*rz);
+                double dr3 = dr * dr * dr;
+                gx += G * rx * bodies[j].mass / dr3;
+                gy += G * ry * bodies[j].mass / dr3;
+                gz += G * rz * bodies[j].mass / dr3;
+            }
+            bodies[i].vel[0] += gx*dt;
+            bodies[i].vel[1] += gy*dt;
+            bodies[i].vel[2] += gz*dt;
+            """,
+            "update_velocity", preamble=body_struct_c
+        )
+
+        self.updatePositions = ElementwiseKernel(self.ctx, 
+            "body *bodies, double dt",
+            """
+            bodies[i].pos[0] += bodies[i].vel[0]*dt;
+            bodies[i].pos[1] += bodies[i].vel[1]*dt;
+            bodies[i].pos[2] += bodies[i].vel[2]*dt;
+            """,
+            "update_positions", preamble=body_struct_c
+        )
+
+
+    def LeapFrog(self, b, dt, n_steps, G):
         bodies = cl.array.to_device(self.queue, b)
-        acc = cl.array.empty(self.queue, (len(b), 3), dtype=np.float32)
-        accelerations(len(bodies), bodies, acc)
-        return acc.get()
+        # Half timestep
+        self.updateVelocities(len(b), bodies, G, -dt/2).wait()
+        for i in tqdm(range(n_steps)):
+            self.updateVelocities(len(b), bodies, G, dt).wait()
+            self.updatePositions(bodies, dt).wait()
+        self.updateVelocities(len(b), bodies, G, dt/2).wait()
+        return bodies.get()
+    
+    def LeapFrogSave(self, b, dt, n_steps, G, every_step=1):
+        saves = np.empty(((n_steps-1)//every_step+1, len(b)), dtype=body)
+        bodies = cl.array.to_device(self.queue, b)
+        # Half timestep
+        self.updateVelocities(len(b), bodies, G, -dt/2)
+        for i in tqdm(range(n_steps)):
+            self.updateVelocities(len(b), bodies, G, dt)
+            self.updatePositions(bodies, dt)
+            if i % every_step == 0:
+                saves[i//every_step] = bodies.get()
+        self.updateVelocities(len(b), bodies, G, dt/2)
+        return saves
+
+
+
