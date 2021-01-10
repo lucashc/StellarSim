8d80cbb8249be7d3af596d0c7be6e40c61fc18c0
PimKeer <pim.keer@gmail.com>
2021-01-10 17:50:54 +0100

    Modifications to PI radial profile.

--- helper_files/DMRadDistNew.py
+++ helper_files/DMRadDistNew.py
@@ -30,21 +30,21 @@ def NFWradSample(size=1, rho0=1, Rs=scale, c=edge):
 
 
 
-def PIradPDF(r, rho0=1, Rc=scale, R_halo=edge):
+def PIradPDF(r, Rc=scale, R_halo=edge):
     """Pseudo-isothermal probability density function for radial distance of dark matter particles."""
     normalise = (4 * np.pi * Rc ** 3 * (R_halo / Rc - np.arctan(R_halo / Rc)))
     if r < R_halo:
-        return rho0/(1+(r/Rc)**2)*4*np.pi*r**2/normalise
+        return 1/(1+(r/Rc)**2)*4*np.pi*r**2/normalise
     else:
         return 0
 
 
-def PIradSample(size, rho0=1, Rc=scale, R_halo = edge):
+def PIradSample(size, Rc=scale, R_halo = edge):
       """Returns n samples from radial distribution described by PIradPDF."""
       samples = []
       while len(samples) < size:
           r = np.random.uniform(low=0, high=R_halo)
-          prop = PIradPDF(r, rho0, Rc, R_halo)
+          prop = PIradPDF(r, Rc, R_halo)
           if np.random.uniform(low=0, high=1) <= prop:
               samples += [r]
 
@@ -61,8 +61,15 @@ plt.ylabel("$f_{R_c,R_d}(r)$")
 plt.grid()
 plt.show()
 
-sample = PIradSample(10000)
-binwidth=1e19
-plt.hist(sample,bins=np.arange(min(sample), max(sample) + binwidth, binwidth))
+a=12
+x = np.linspace(0.001,a,1000*a)
+y = np.zeros(len(x))
+for i in range(len(x)):
+    y[i] = PIradPDF(x[i])
+plt.plot(x,y,color="red")
+
+sample = PIradSample(10000)/sc.RCmw
+binwidth=1e19/sc.RCmw
+plt.hist(sample,bins=np.arange(min(sample), max(sample) + binwidth, binwidth),density=True)
 plt.show()
 """
