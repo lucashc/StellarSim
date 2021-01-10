0c1ce28a3d51c75294e4487d30024338a5b5099a
PimKeer <pim.keer@gmail.com>
2021-01-10 17:35:28 +0100

    Updated NFW and PI radial profiles.

---
+++ helper_files/DMRadDistNew.py
@@ -0,0 +1,68 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import helper_files.stellarConstants as sc
+import time
+
+scale = 1
+edge = 12
+
+def NFWradPDF(r, rho0=1, Rs=scale, c=edge):
+    """Navarro-Frenk-White probability density function for radial distance of dark matter particles."""
+    normalise = 4*np.pi*rho0*Rs**3*(np.log(1+c)-c/(1+c))
+    if r < c*Rs:
+        p = rho0*Rs/(r*(1+r/Rs)**2)*4*np.pi*r**2
+        return p/normalise
+    else:
+        return 0
+
+
+def NFWradSample(size=1, rho0=1, Rs=scale, c=edge):
+      """Returns n samples from radial distribution described by DMradPDF."""
+      samples=[]
+      while len(samples) < size:
+          r = np.random.uniform(low=0,high=c*Rs)
+          prop = NFWradPDF(r, rho0, Rs, c)
+          if np.random.uniform(low=0,high=1) <= prop:
+              samples += [r]
+
+      return np.array(samples)*sc.RCmw
+
+
+
+
+def PIradPDF(r, rho0=1, Rc=scale, R_halo=edge):
+    """Pseudo-isothermal probability density function for radial distance of dark matter particles."""
+    normalise = (4 * np.pi * Rc ** 3 * (R_halo / Rc - np.arctan(R_halo / Rc)))
+    if r < R_halo:
+        return rho0/(1+(r/Rc)**2)*4*np.pi*r**2/normalise
+    else:
+        return 0
+
+
+def PIradSample(size, rho0=1, Rc=scale, R_halo = edge):
+      """Returns n samples from radial distribution described by PIradPDF."""
+      samples = []
+      while len(samples) < size:
+          r = np.random.uniform(low=0, high=R_halo)
+          prop = PIradPDF(r, rho0, Rc, R_halo)
+          if np.random.uniform(low=0, high=1) <= prop:
+              samples += [r]
+
+      return np.array(samples) * sc.RCmw
+"""
+a=edge
+x = np.linspace(0.001,a,1000*a)
+y = np.zeros(len(x))
+for i in range(len(x)):
+    y[i] = PIradPDF(x[i])
+plt.plot(x,y,color="red")
+plt.xlabel("$r$ (in units of $R_c$)")
+plt.ylabel("$f_{R_c,R_d}(r)$")
+plt.grid()
+plt.show()
+
+sample = PIradSample(10000)
+binwidth=1e19
+plt.hist(sample,bins=np.arange(min(sample), max(sample) + binwidth, binwidth))
+plt.show()
+"""
