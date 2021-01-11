02cf41288d74ba810fcc40b63701da27ce537648
Lucas Crijns <lucascrijns@gmail.com>
2021-01-11 19:19:35 +0100

    Custom GPU program

--- GPU/GPU.py
+++ GPU/GPU.py
@@ -15,70 +15,32 @@ class GPU:
         body_struct, body_struct_c = cl.tools.match_dtype_to_c_struct(self.ctx.devices[0], 'body', body)
         cl.tools.get_or_register_dtype('body', body_struct)
 
-        # kernels
-        self.getAccelerations = ElementwiseKernel(self.ctx, 
-            "int n_bodies, body *bodies, double *acc, double G",
-            """
-            double gx = 0, gy = 0, gz = 0;
-            for (int j = 0; j < n_bodies; j++) {
-                if (i == j) continue;
-                double rx = bodies[j].pos[0]-bodies[i].pos[0];
-                double ry = bodies[j].pos[1]-bodies[i].pos[1];
-                double rz = bodies[j].pos[2]-bodies[i].pos[2];
-                double dr = rsqrt(rx*rx + ry*ry + rz*rz);
-                double dr3 = dr * dr * dr;
-                gx += G * rx * bodies[j].mass * dr3;
-                gy += G * ry * bodies[j].mass * dr3;
-                gz += G* rz * bodies[j].mass * dr3;
-            }
-            acc[i*3 + 0] = gx;
-            acc[i*3 + 1] = gy;
-            acc[i*3 + 2] = gz;
-            """,
-            "get_accelerations", preamble=body_struct_c
-        )
+        with open('./GPU/program.cl', 'r') as f:
+            source = f.read()
+        self.program = cl.Program(self.ctx, body_struct_c + source).build()
 
-        self.updateVelocities = ElementwiseKernel(self.ctx, 
-            "int n_bodies, body *bodies, double G, double dt",
-            """
-            double gx = 0, gy = 0, gz = 0;
-            for (int j = 0; j < n_bodies; j++) {
-                if (i == j) continue;
-                double rx = bodies[j].pos[0]-bodies[i].pos[0];
-                double ry = bodies[j].pos[1]-bodies[i].pos[1];
-                double rz = bodies[j].pos[2]-bodies[i].pos[2];
-                double dr = sqrt(rx*rx + ry*ry + rz*rz);
-                double dr3 = dr * dr * dr;
-                gx += G * rx * bodies[j].mass / dr3;
-                gy += G * ry * bodies[j].mass / dr3;
-                gz += G * rz * bodies[j].mass / dr3;
-            }
-            bodies[i].vel[0] += gx*dt;
-            bodies[i].vel[1] += gy*dt;
-            bodies[i].vel[2] += gz*dt;
-            """,
-            "update_velocity", preamble=body_struct_c
-        )
-
-        self.updatePositions = ElementwiseKernel(self.ctx, 
-            "body *bodies, double dt",
-            """
-            bodies[i].pos[0] += bodies[i].vel[0]*dt;
-            bodies[i].pos[1] += bodies[i].vel[1]*dt;
-            bodies[i].pos[2] += bodies[i].vel[2]*dt;
-            """,
-            "update_positions", preamble=body_struct_c
-        )
-
+    def update_velocities(self, bodies, G, dt):
+        return self.program.update_velocities(self.queue, (len(bodies),), None, np.int32(len(bodies)), bodies.data, np.double(G), np.double(dt))
+    
+    def update_positions(self, bodies, dt):
+        return self.program.update_velocities(self.queue, (len(bodies),), None, bodies.data, np.double(dt))
+    
+    def get_accelerations(self, bodies, G):
+        bodies = cl.array.to_device(self.queue, bodies)
+        acc = cl.array.zeros(self.queue, (len(bodies), 3), np.double)
+        self.program.get_accelerations(self.queue, (len(bodies),), None, np.int32(len(bodies)), bodies.data, acc.data, np.double(G))
+        return acc.get()
+    
+    def update(self, bodies, G, dt):
+        return self.program.update(self.queue, (len(bodies),), None, np.int32(len(bodies)), bodies.data, np.double(G), np.double(dt))
 
     def LeapFrog(self, b, dt, n_steps, G):
         bodies = cl.array.to_device(self.queue, b)
         # Half timestep
-        self.updateVelocities(len(b), bodies, G, -dt/2).wait()
+        self.update_velocities(bodies, G, -dt/2).wait()
         for i in tqdm(range(n_steps)):
-            self.updateVelocities(len(b), bodies, G, dt).wait()
-            self.updatePositions(bodies, dt).wait()
-        self.updateVelocities(len(b), bodies, G, dt/2).wait()
+            self.update(bodies, G, dt).wait()
+        self.update_velocities(bodies, G, dt/2).wait()
         return bodies.get()
     
     def LeapFrogSave(self, b, dt, n_steps, G, every_step=1):
--- test_GPU.py
+++ test_GPU.py
@@ -1,5 +1,7 @@
 from GPU.GPU import GPU
 from GPU.types import make_body_array
+from time import time
+from sys import getsizeof
 
 import helper_files.RadDist as RadDist
 import helper_files.stellarConstants as sc
@@ -8,37 +10,49 @@ import helper_files.sim_utils as utils
 import barneshut_cpp.cppsim as cs
 import helper_files.plotting as plotting
 
+cs.set_thread_count(8)
+
 
 g = GPU()
 
 def genStableGalaxy(n_stars, m_star, m_bh):
     masses = np.array([m_bh] + [m_star]*n_stars)
-    r = np.sort(RadDist.radSample(size=n_stars))
+    r = np.sort(RadDist.radSample(size=n_stars, length_guess=5))
     theta = np.random.uniform(0, 2*np.pi, n_stars)
-    x = r*np.cos(theta)
-    y = r*np.sin(theta)
-    z = np.zeros(n_stars)
-    positions = np.column_stack((x, y, z))
+    positions = np.column_stack((r*np.cos(theta), r*np.sin(theta), np.zeros(n_stars)))
     v_norm = np.sqrt(sc.G*np.cumsum(masses)[:-1]/np.linalg.norm(positions, axis=1))  # skip first cumsum value, since masses includes black hole
     v_unit_vec = np.column_stack((-np.sin(theta), np.cos(theta), np.zeros(n_stars)))
     velocities = v_norm.reshape((n_stars, 1)) * v_unit_vec
     positions = np.insert(positions, 0, np.zeros(3), 0)     # add black hole (already present in masses)
     velocities = np.insert(velocities, 0, -np.sum(velocities*m_star, axis=0)/m_bh, 0)
+    print("-----")
+    print('\n'.join([str((var, getsizeof(a))) for var, a in locals().items()]))
     # print(velocities)
     # print(velocities*np.expand_dims(masses, axis=0).T)
     # print(np.sum(velocities*np.expand_dims(masses, axis=0).T, axis=0))
-    return make_body_array(positions, velocities, masses)
+    return positions, velocities, masses
 
 
 thetamax = 0.7
 n_steps = 2001
 m_star = sc.Msol  # 3.181651515706176e+30
-galaxy = genStableGalaxy(int(1e5), m_star*10, sc.Msgra)
-M = 5000*m_star*10 + sc.Msgra
-# cs.LeapFrogC(galaxy, 1e12, 5000, thetamax, sc.G)
-print("Done with step 1")
-result = g.LeapFrog(galaxy, dt=1e12, n_steps=n_steps, G=sc.G)
-print(result)
-bl = utils.zip_to_bodylist(result['pos'], result['vel'], result['mass'])
-result = cs.LeapFrogSaveC(bl, 0, 1)
-result.save("gpu.binv")
+n_star = int(1e5)
+pos, vel, mass = genStableGalaxy(n_star, m_star*10, sc.Msgra)
+
+galaxy1 = make_body_array(pos, vel, mass)
+galaxy2 = utils.zip_to_bodylist(pos, vel, mass)
+
+del pos, vel, mass
+
+
+# t1 = time()
+# result = g.LeapFrog(galaxy1, dt=1e12, n_steps=n_steps, G=sc.G)
+# t2 = time()
+
+# print(f"GPU: {t2-t1} s")
+
+t1 = time()
+cs.LeapFrogC(galaxy2, dt=1e12, n_steps=n_steps, G=sc.G, thetamax=0.7)
+t2 = time()
+
+print(f"CPU: {t2-t1} s")
