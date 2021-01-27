4c419a00555fd582c99c4269b731b45c1ead29a7
Lucas Crijns <lucascrijns@gmail.com>
2021-01-27 22:11:55 +0100

    Collisions.py update

--- Scenarios/Collision.py
+++ Scenarios/Collision.py
@@ -1,6 +1,7 @@
 from helper_files.galaxy_creator import create_andromeda, create_milky_way
 import barneshut_cpp.cppsim as cs
 import helper_files.stellarConstants as sc
+import numpy as np
 
 cs.set_thread_count(8)
 
@@ -10,9 +11,14 @@ MW = cs.Result.load_last("MWMature.binv")
 AM = cs.Result.load_last("AMMature.binv")
 
 # Set collision course
+MW.translate(np.array([-1, 0, 0])*sc.ly*1e6/1.5)
+MW.add_velocity(np.array([1, 0, 0])*225e3/2)
 
+AM.rotate(np.pi/6, np.zeros(3, dtype=np.double), np.array([1,1,0], dtype=np.double))
+AM.translate(np.array([1, 0, 0])*sc.ly*1e6/1.5)
+AM.add_velocity(np.array([-1, 0, 0])*225e3/2)
 
 CC = MW + AM
 
-result = cs.LeapFrogSaveC(CC, dt=1e12, n_steps=10000, thetamax=0.7, G=sc.G, save_every=100, epsilon=4e16)
+result = cs.LeapFrogSaveC(CC, dt=1e13, n_steps=10000, thetamax=0.7, G=sc.G, save_every=100, epsilon=4e16)
 result.save("Collision.binv")
